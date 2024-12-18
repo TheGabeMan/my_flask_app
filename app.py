8997e2a8d096ea23c032e44c2b35aeeadced8c17
Gabrie van Zanten <thegabeman@gmail.com>
2024-12-18 09:33:16 +0000

    form parse working

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+
 app = Flask(__name__,template_folder="./templates")
-
-
-
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/reports")
+@app.route("/reports", methods=["GET","POST"])
 def reports():
-     return render_template("reports.html")
-
-@app.route("/settings")
-def settings():
-     return render_template("settings.html")
+     if request.method == "POST":
+          period = request.form.get('month-year')
+          return render_template("reports.html", period=period)
+     else:
+          return render_template("reports.html")
 
 if __name__ in "__main__":
     app.run(debug=True)
